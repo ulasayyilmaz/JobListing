9b80e08f48ac15aad1d2d74c747d69c5c563f415
Ulas Ayyilmaz <iuaa2020@mymail.pomona.edu>
2023-03-20 10:48:10 +0300

    Ulas's Webpage + Jobs Listing with stying

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+JOBS = [
+    {
+        'id': "Accountant", 
+        'salary': "$100,000",
+        'location': "New York"
+    },
+    {
+        'id': "Software Developer", 
+        'salary': "$160,000",
+        'location': "San Francisco"
+    },
+    {
+        'id': "Product Manager", 
+        'salary': "$120,000",
+        'location': "Mountain View"
+    }]
+
+app = Flask(__name__)
+@app.route("/")
+def helloWorld():
+    return render_template('home.html', jobs = JOBS)
